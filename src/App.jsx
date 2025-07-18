efe0502e9af42e971f15e48ee0585351376e3761
Jaysinghgautam <gjaysingh53@gmail.com>
2025-07-18 08:06:53 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,46 @@
- import React from 'react'
- 
- function App() {
-   return (
-     <div>
-      <h1 className='flex justify-center'>welcome to my portfolio</h1>
+import React from "react";
 
-     </div>
-   )
- }
- 
- export default App
+function App() {
+  return (
+    <div>
+      <h1 className="flex justify-center">welcome to my portfolio</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+        voluptate fugiat delectus velit blanditiis maiores. Neque optio amet
+        nesciunt natus eveniet. Minima qui ipsam quas deleniti, rem earum dicta
+        dolore. Perferendis nisi, quas accusantium repellat aut saepe laboriosam
+        et architecto minus vero tempora laborum. Ipsa autem cupiditate deserunt
+        repellat atque tempore, architecto aliquam aliquid libero odit eius,
+        non, excepturi sed! Inventore ab minima neque aliquid voluptate! Aperiam
+        molestias quisquam fugit nisi quidem animi cupiditate placeat laudantium
+        eos ipsum, optio sapiente sunt soluta eligendi quia debitis officia
+        quaerat porro velit doloribus! Vero debitis impedit alias porro, velit
+        temporibus aspernatur maxime repellendus sit consectetur veniam nisi
+        repudiandae laboriosam, iste itaque aliquid eligendi ea quisquam numquam
+        magnam beatae incidunt cum, nemo aut! Iusto. Voluptates officia nemo rem
+        aliquid aperiam? Quidem accusantium sint pariatur, architecto vero
+        aspernatur officia fuga blanditiis quas praesentium eligendi
+        necessitatibus repellendus labore quod recusandae error odit iure
+        impedit magnam numquam. Modi reprehenderit a placeat iusto nesciunt id
+        nihil magni cupiditate vel doloremque, maiores impedit commodi odio rem
+        officiis? Quos cumque, dolorum fugiat aspernatur ullam est officiis
+        eligendi voluptas velit laborum? Fuga ipsum, perferendis corrupti
+        possimus odit dignissimos praesentium molestias nisi perspiciatis veniam
+        qui, minima eveniet similique. Amet repellat, harum, expedita illo quasi
+        veritatis eaque commodi magnam deserunt, accusantium impedit non!
+        Temporibus est molestias, aliquam qui necessitatibus odit officiis. In,
+        doloribus illo neque eius explicabo sapiente aliquam omnis velit impedit
+        assumenda dolores excepturi, ullam provident architecto officiis sit
+        mollitia ex iusto. Numquam sapiente dolorum magni nostrum alias in?
+        Molestiae modi eveniet nesciunt. Dolorum sunt corrupti, quasi similique
+        eligendi eius ab quos, eum ratione iste vel quae, fugiat assumenda earum
+        reprehenderit quisquam! Facilis, aspernatur! Quidem ducimus at
+        necessitatibus exercitationem sapiente aliquam fuga enim veniam aliquid
+        inventore molestiae, a dolores deleniti tenetur? At, ab quae illum quis
+        eos iusto quisquam nemo ducimus ipsum.
+      </p>
+    </div>
+  );
+}
+
+export default App;
