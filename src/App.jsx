1d9d44c6b73dd2262ce256265944cb63a9c853e8
Jaysinghgautam <gjaysingh53@gmail.com>
2025-07-18 07:42:32 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+ import React from 'react'
+ 
+ function App() {
+   return (
+     <div>
+      <h1 className='flex justify-center'>welcome to my portfolio</h1>
+
+     </div>
+   )
+ }
+ 
+ export default App
